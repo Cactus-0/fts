b38756f239972e675f77d2776e245aeda0159269
CactusDev <name79412@gmail.com>
2022-06-08 09:37:13 +0300

    [v1.3.1] removed useless dependecies

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
 import path from 'path';
-import { address } from 'ip';
 
 export namespace Constants {
     export const CONFIG_FILE_NAME = 'fts-config.json';
     export const CONFIG_FILE_PATH = path.join(__dirname, Constants.CONFIG_FILE_NAME);
-    export const IP = address();
 }
